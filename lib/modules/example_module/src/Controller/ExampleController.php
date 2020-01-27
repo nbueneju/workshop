08c5e0fa14664fc9a773a3540fd71853e2ff83cd
nbueneju <juan.pedro.buenestado.perez@everis.com>
2020-01-27 12:15:12 +0000

    Initial commit

---
+++ lib/modules/example_module/src/Controller/ExampleController.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @file
+ * Contains \Drupal\example_module\Controller\ExampleController.
+ */
+ 
+namespace Drupal\example_module\Controller;
+ 
+use Drupal\Core\Controller\ControllerBase;
+ 
+class ExampleController extends ControllerBase {
+  public function content() {
+    return array(
+      '#type' => 'markup',
+      '#markup' => t('Hello, this is an example'),
+    );
+  }
+}
